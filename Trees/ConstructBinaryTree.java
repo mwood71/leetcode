5a89a04af5a5f6fb8d1474c4a2dd7d985ded5591
mwood71 <awood9976@gmail.com>
2022-09-07 15:30:31 -0500

    modified construct binary tree

--- Trees/ConstructBinaryTree.java
+++ Trees/ConstructBinaryTree.java
@@ -1,43 +1,44 @@
 package Leetcode.Trees;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ConstructBinaryTree {
-    int index = 0;
-    int[] inorder;
-    int[] preorder;
     public static void main(String[] args) {
         ConstructBinaryTree constructBinaryTree = new ConstructBinaryTree();
-        int[] inorder = new int[]{3,2,1};
-        int[] preorder = new int[]{1,2,3};
+        int[] inorder = new int[]{9,3,15,20,7};
+        int[] preorder = new int[]{3,9,20,15,7};
         TreeNode result = constructBinaryTree.buildTree(preorder, inorder);
         print(result);
     }
 
+    int index = 0;
+    int[] preorder;
+    Map<Integer, Integer> hm;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < inorder.length; i++) {
-            map.put(inorder[i], i);
-        }
-        this.inorder = inorder;
         this.preorder = preorder;
-        return dfs(0, preorder.length - 1, map);
+        Map<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            hm.put(inorder[i], i);
+        }
+        this.hm = hm;
+
+        return dfs(0,preorder.length-1);
     }
 
-    TreeNode dfs(int left, int right, HashMap<Integer, Integer> map) {
-        int currValue = preorder[index++];
-        int currIndex = (int) map.get(currValue);
-        TreeNode root = new TreeNode(currValue);
+    TreeNode dfs(int a, int b) {
+        if (a > b) return null;
 
-        if (currIndex > left) {
-            root.left = dfs(left, currIndex-1, map);
-        }
-        if (currIndex < right) {
-            root.right = dfs(currIndex+1, right, map);
-        }
-        return root;
+        int currNum = preorder[index];
+        index++;
+        int inorderIndex = hm.get(currNum);
+
+        TreeNode currNode = new TreeNode(currNum);
+
+        currNode.left = dfs(a, inorderIndex-1);
+        currNode.right = dfs(inorderIndex+1, b);
+        return currNode;
     }
-
 
     public static void print(TreeNode node) {
         if (node == null) return;
